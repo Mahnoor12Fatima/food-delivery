608c5ee63037649857ec679da002bd975e832607
Mahnoor Fatima <100063303+Mahnoor12Fatima@users.noreply.github.com>
2026-07-17 01:19:53 -0700

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -14,7 +14,7 @@ const FoodDisplay = ({ category }) => {
 
       {/* Food items grid */}
       <div className="mt-8 grid gap-8 md:gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-fr">
-        {food_list.map((item, index) => {
+       {food_list?.map((item, index) => {
           if (category === "All" || category === item.category) {
             return (
               <FoodItem
